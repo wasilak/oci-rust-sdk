3632e56377476c837a7a3b9bf6de3e068be84baa
Piotr Boruc <piotr.m.boruc@gmail.com>
2025-02-27 20:28:58 +0100

    updates

--- src/vault_secret.rs
+++ src/vault_secret.rs
@@ -1,24 +1,38 @@
 use crate::base_client::{encode_body, oci_signer};
 use crate::identity::Identity;
+use base64::{engine::general_purpose, Engine as _};
 use chrono::{DateTime, Utc};
 use reqwest::header::HeaderMap;
-use reqwest::Response;
+use serde::Deserialize;
 use serde_json::json;
+use std::collections::HashMap;
 
-pub struct VaultSecret {
+pub struct Vault {
     identity: Identity,
 }
 
-impl VaultSecret {
+#[derive(Clone, Debug, Deserialize, Default)]
+#[serde(rename_all = "camelCase")]
+struct VaultSecretResponse {
+    secret_bundle_content: VaultSecret,
+}
+
+#[derive(Deserialize, Debug, Clone, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct VaultSecret {
+    pub content: String,
+}
+
+impl Vault {
     pub fn new(identity: Identity) -> Self {
-        VaultSecret { identity }
+        Vault { identity }
     }
 
     pub async fn get_secret(
         &self,
         secret_name: &str,
         vault_ocid: &str,
-    ) -> Result<Response, Box<dyn std::error::Error>> {
+    ) -> Result<VaultSecret, Box<dyn std::error::Error>> {
         let url = format!(
             "https://secrets.vaults.{}.oci.oraclecloud.com",
             self.identity.get_auth_config().region
@@ -71,6 +85,25 @@ impl VaultSecret {
             .send()
             .await?;
 
-        return Ok(response);
+        let secret_content: VaultSecretResponse = response.json::<VaultSecretResponse>().await?;
+
+        return Ok(secret_content.secret_bundle_content);
+    }
+}
+
+impl VaultSecret {
+    pub fn decode(&self) -> String {
+        match general_purpose::STANDARD.decode(&self.content) {
+            Ok(decoded) => match String::from_utf8(decoded) {
+                Ok(decoded_str) => decoded_str,
+                Err(e) => format!("Error decoding secret: {:?}", e),
+            },
+            Err(e) => format!("Error decoding secret: {:?}", e),
+        }
+    }
+
+    pub async fn get_json(&self) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
+        let secret_content: HashMap<String, String> = serde_json::from_str(&self.decode())?;
+        Ok(secret_content)
     }
 }
